e87baa1e6fb852f62580438fe43c1ca8406585d6
Balazs Zubak <balazs.zubak@doclerholding.com>
2016-05-31 16:09:52 +0200

    removed unused imports

--- test/MainMachinaTest.hx
+++ test/MainMachinaTest.hx
@@ -1,14 +1,14 @@
 package;
 
+import hex.unittest.notifier.TraceNotifier;
 import hex.unittest.runner.ExMachinaUnitCore;
 import hex.unittest.runner.TestRunner;
-
-#if flash
-import flash.Lib;
-import hex.unittest.notifier.TraceNotifier;
-#else
+#if js
 import hex.unittest.notifier.ConsoleNotifier;
+#elseif flash
+import flash.Lib;
 #end
+
 
 /**
  * ...
@@ -18,13 +18,16 @@ class MainMachinaTest
 {
 	static public function main() : Void
 	{
+		
 		var emu = new ExMachinaUnitCore();
         
 		#if flash
 		TestRunner.RENDER_DELAY = 0;
 		emu.addListener( new TraceNotifier( Lib.current.loaderInfo ) );
-		#else
+		#elseif js
 		emu.addListener( new ConsoleNotifier( false ) );
+		#else
+		emu.addListener( new TraceNotifier( false ) );
 		#end
 		
         emu.addTest( MachinaSuite );
