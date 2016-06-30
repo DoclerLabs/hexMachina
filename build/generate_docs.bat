62617270ddf3f9271b6730de3fdacaebc2ba4979
David Klausz <david.klausz@doclerholding.com>
2016-06-30 14:18:04 +0200

    New design init

--- build/generate_docs.bat
+++
@@ -1,6 +0,0 @@
-haxelib newrepo
-haxelib install hexmachina
-haxelib git dox https://github.com/HaxeFoundation/dox.git
-
-haxe doc_build.hxml
-haxelib run dox -i ./bin/docs/ -o ../docs --title "hexMachina API" --toplevel-package hex -in hex -t ./dox/templates
