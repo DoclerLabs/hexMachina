421ff586f91ddba17885461b2d90a9c8b8e2ccae
Laurent Deketelaere <laurent@geturl.net>
2016-09-20 09:50:15 +0200

    merge sources in bundle

--- release-hexMachina-bundle.sh
+++ release-hexMachina-bundle.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 cd $HOME
-zip -r ./hexMachina-bundle-${TRAVIS_TAG}.zip ./hexcore/src ./hexcore/test ./hexcore/README.md ./hexinject/src ./hexinject/test ./hexinject/README.md ./hexmvc/src ./hexmvc/test ./hexmvc/README.md ./hexioc/src ./hexioc/test ./hexioc/README.md ./hexservice/src ./hexservice/test ./hexservice/README.md ./hexannotation/src ./hexannotation/test ./hexannotation/README.md ./hexstate/src ./hexstate/test ./hexstate/README.md ./hexunit/src ./hexunit/test ./hexunit/README.md 
+mkdir hexmachina-bundle hexmachina-bundle/src hexmachina-bundle/test
+rsync -a ./hexcore/src ./hexmachina-bundle/src 
+rsync -a ./hexcore/test ./hexmachina-bundle/test 
+rsync -a ./hexinject/src ./hexmachina-bundle/src 
+rsync -a ./hexinject/test ./hexmachina-bundle/test 
+rsync -a ./hexmvc/src ./hexmachina-bundle/src 
+rsync -a ./hexmvc/test ./hexmachina-bundle/test 
+rsync -a ./hexioc/src ./hexmachina-bundle/src 
+rsync -a ./hexioc/test ./hexmachina-bundle/test 
+rsync -a ./hexservice/src ./hexmachina-bundle/src 
+rsync -a ./hexservice/test ./hexmachina-bundle/test 
+rsync -a ./hexannotation/src ./hexmachina-bundle/src 
+rsync -a ./hexannotation/test ./hexmachina-bundle/test 
+rsync -a ./hexstate/src ./hexmachina-bundle/src 
+rsync -a ./hexstate/test ./hexmachina-bundle/test 
+rsync -a ./hexunit/src ./hexmachina-bundle/src 
+rsync -a ./hexunit/test ./hexmachina-bundle/test 
+zip -r ./hexMachina-bundle-${TRAVIS_TAG}.zip  ./hexmachina-bundle
 mv ./hexMachina-bundle-${TRAVIS_TAG}.zip $TRAVIS_BUILD_DIR
