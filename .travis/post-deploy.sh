44647ea0eec56cbb6e96ba48709947e9f6f3a402
Laurent Deketelaere <laurent@geturl.net>
2016-09-22 13:59:28 +0200

    Update post-deploy.sh

--- .travis/post-deploy.sh
+++ .travis/post-deploy.sh
@@ -3,7 +3,7 @@ echo "checkout gh-pages branch"
 git clone https://github.com/aliokan/hexMachina.git ~/hexMachina
 cd ~/hexMachina
 git checkout gh-pages
-echo "updpate downloads.yml content."
+echo "update downloads.yml content."
 node $TRAVIS_BUILD_DIR/.travis/update-download-list.js ~/hexMachina
 echo "commit change."
 git status
