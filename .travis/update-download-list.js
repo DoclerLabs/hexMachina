32b3b437b1584f67f4fc23de68b27bd6149710c8
Laurent Deketelaere <laurent@geturl.net>
2016-09-22 14:55:37 +0200

    with node 6.6 is better

--- .travis/update-download-list.js
+++ .travis/update-download-list.js
@@ -2,7 +2,7 @@ const https = require('https');
 const fs = require('fs');
 
 var gitPath = "";
-process.argv.forEach( function(val, index){
+process.argv.forEach((val, index) => {
   //console.log(`${index}: ${val}`);
   if(index == 2) gitPath = val;
 });
@@ -17,28 +17,28 @@ var options = {
   headers: {'user-agent': 'node.js'}
 };
 
-return https.get(options, function(res) {
-
+return https.get(options, (res) => {
+  
   var body = '';
-  res.on('data', function(chunk) {
+  res.on('data', (chunk) => {
     //console.log(`BODY: ${chunk}`);
     body += chunk;
   });
-  res.on('end', function() {
+  res.on('end', () => {
     console.log('No more data in response.');
     var downloads = buildDownloadList( body );
     console.log('No more data in response.');
     saveContent(downloadsFilePath, downloads);
   });
-}).on('error', function(err) {
-  console.error("Got error: " + err.message);
+}).on('error', (err) => {
+  console.error(`Got error: ${err.message}`);
 });
 
 function buildDownloadList(json)
 {
   try {
     var releases = JSON.parse(json);
-
+    
     var release, asset, log;
     downloads = "";
     for (var i = 0; i < releases.length; i++)
@@ -50,8 +50,8 @@ function buildDownloadList(json)
         asset = release.assets[j];
         if(asset.name.lastIndexOf('hexMachina-bundle') != -1)
         {
-          downloads += "- name: Download hexMachina v" + release.tag_name + "\n";
-          downloads += "  link: " + asset.browser_download_url + "\n";
+          downloads += `- name: Download hexMachina v${release.tag_name}\n`;
+          downloads += `  link: ${asset.browser_download_url}\n`;
           //log.push(release.tag_name);
           //log.push(asset.name);
           //log.push(asset.browser_download_url);
@@ -60,20 +60,19 @@ function buildDownloadList(json)
     }
     return downloads;
   } catch (err) {
-    console.error("Unable to parse response as JSON " + err.message);
+    console.error(`Unable to parse response as JSON ${err.message}`);
   }
 }
 
 function saveContent(path, text)
 {
   console.log(path, text);
-  var exist = fs.existsSync(path);
-  //var err = fs.accessSync(path, fs.constants.R_OK | fs.constants.W_OK);
-  if(exist)
+  var err = fs.accessSync(path, fs.constants.R_OK | fs.constants.W_OK);
+  if(!err)
   {
     fs.writeFileSync(path, text);
   } else
   {
-    throw new Error("cannot acces on file.")
+    throw new Error(`cannot acces on file : ${err}`)
   }
 }
