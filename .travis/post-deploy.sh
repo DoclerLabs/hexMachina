b6b887fbf3ac25c085c3b2cadb19d097e6dc032e
Laurent Deketelaere <laurent@geturl.net>
2016-09-22 13:53:11 +0200

    Update post-deploy.sh

--- .travis/post-deploy.sh
+++ .travis/post-deploy.sh
@@ -5,6 +5,7 @@ cd ~/hexMachina
 git checkout gh-pages
 echo "updpate downloads.yml content."
 node $TRAVIS_BUILD_DIR/.travis/update-download-list.js ~/hexMachina
+less ~/hexMachina/_data/downloads.yml
 echo "commit change."
 git status
 git config credential.helper "store --file=.git/credentials"
