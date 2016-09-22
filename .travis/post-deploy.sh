4e1ab96ee93eb9e8553fca16a7124f1ac2ae926e
Laurent Deketelaere <laurent@geturl.net>
2016-09-22 13:41:43 +0200

    Update post-deploy.sh

--- .travis/post-deploy.sh
+++ .travis/post-deploy.sh
@@ -7,8 +7,8 @@ echo "updpate downloads.yml content."
 node $TRAVIS_BUILD_DIR/.travis/update-download-list.js ~/hexMachina
 echo "commit change."
 git status
-git config --global user.email "laurent@geturl.net"
-git config --global user.name "aliokan"
+git config credential.helper "store --file=.git/credentials"
+echo "https://$GH_TOKEN:@github.com" > .git/credentials
 git commit -m "update downloads.yml"
 git push
 echo "is pushed."
