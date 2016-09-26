c6a408cfb11ea71a943f8f256ac33ca0630f0c28
Christoph Otter <christophotter@gmail.com>
2016-09-26 09:05:17 +0200

    Added documentation generation

--- .travis/post-deploy.sh
+++ .travis/post-deploy.sh
@@ -5,10 +5,15 @@ cd ~/hexMachina
 git checkout gh-pages
 echo "update downloads.yml content."
 node $TRAVIS_BUILD_DIR/.travis/update-download-list.js ~/hexMachina
+echo "delete old documentation."
+git rm -rf ./docs/api
+echo "add new documentation."
+mv $HOME/api ./docs/api
+git add ./docs/api
 echo "commit change."
 git status
 git config credential.helper "store --file=.git/credentials"
 echo "https://$GH_TOKEN:@github.com" > .git/credentials
-git commit -a -m "update downloads.yml"
+git commit -a -m "update downloads.yml and api documentation"
 git push
 echo "is pushed."
