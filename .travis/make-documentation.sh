f74a95cfb05d86ade8c9df566728cd61e56c1919
Laurent Deketelaere <laurent@geturl.net>
2017-02-13 10:41:57 +0100

    Fix documentation for 3.4.0; and generate only one time by build

--- .travis/make-documentation.sh
+++ .travis/make-documentation.sh
@@ -2,9 +2,9 @@
 cd $HOME
 
 #Generate documentation
-haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "ImportAll.run()" --no-output -xml xml/neko.xml -php bin
-haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "ImportAll.run()" --no-output -xml xml/neko.xml -neko bin/docs.n
-haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "ImportAll.run()" --no-output -xml xml/js.xml -js bin/docs.js
-haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "ImportAll.run()" --no-output -xml xml/flash.xml -swf-version 11 -swf bin/docs.swf
+haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "include('hex')" --no-output -xml xml/php.xml -php bin
+haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "include('hex')" --no-output -xml xml/neko.xml -neko bin/docs.n
+haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "include('hex')" --no-output -xml xml/js.xml -js bin/docs.js
+haxe -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdoctheme/src -cp ./hexdsl/src --macro "include('hex')" --no-output -xml xml/flash.xml -swf-version 11 -swf bin/docs.swf
 
 haxelib run dox -i xml -o api --toplevel-package "hex" --title "HEXMACHINA - API" -theme ./hexdoctheme/theme/
