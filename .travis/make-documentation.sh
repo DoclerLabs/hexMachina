cbd83387574652206c7e21770b6243fdd45f9635
Laurent Deketelaere <laurent@geturl.net>
2017-03-06 20:24:02 +0100

    move dox installation (due to newrepo)

--- .travis/make-documentation.sh
+++ .travis/make-documentation.sh
@@ -7,4 +7,6 @@ haxe -cp ./hexcore/src -cp ./hexlog/src -cp ./hexreflection/src -cp ./hexinject/
 haxe -cp ./hexcore/src -cp ./hexlog/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdsl/src --macro "include('hex')" -D doc-gen --no-output -xml xml/js.xml -js bin/docs.js
 haxe -cp ./hexcore/src -cp ./hexlog/src -cp ./hexreflection/src -cp ./hexinject/src -cp ./hexmvc/src -cp ./hexioc/src -cp ./hexservice/src -cp ./hexannotation/src -cp ./hexstate/src -cp ./hexunit/src -cp ./hexdsl/src --macro "include('hex')" -D doc-gen --no-output -xml xml/flash.xml -swf-version 11 -swf bin/docs.swf
 
+haxelib install dox
+
 haxelib run dox -i xml -o api --toplevel-package "hex" --title "HEXMACHINA - API" -theme ./hexdoctheme/theme/
