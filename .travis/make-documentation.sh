4381686d5cf9cb957fbedede76cc31daa50891a2
Laurent Deketelaere <laurent@geturl.net>
2018-04-30 15:11:56 +0200

    fix doc generation

--- .travis/make-documentation.sh
+++ .travis/make-documentation.sh
@@ -2,10 +2,10 @@
 cd $HOME
 
 # Generate documentation
-haxe -cp ./hexlog/src -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexannotation/src -cp ./hexinject/src -cp ./hexdsl/src -cp ./hexcommand/src -cp ./hexunit/src --macro "include('hex')" -D doc-gen --no-output -xml xml/php.xml -php bin
-haxe -cp ./hexlog/src -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexannotation/src -cp ./hexinject/src -cp ./hexdsl/src -cp ./hexcommand/src -cp ./hexunit/src --macro "include('hex')" -D doc-gen --no-output -xml xml/neko.xml -neko bin/docs.n
-haxe -cp ./hexlog/src -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexannotation/src -cp ./hexinject/src -cp ./hexdsl/src -cp ./hexcommand/src -cp ./hexunit/src --macro "include('hex')" -D doc-gen --no-output -xml xml/js.xml -js bin/docs.js
-haxe -cp ./hexlog/src -cp ./hexcore/src -cp ./hexreflection/src -cp ./hexannotation/src -cp ./hexinject/src -cp ./hexdsl/src -cp ./hexcommand/src -cp ./hexunit/src --macro "include('hex')" -D doc-gen --no-output -xml xml/flash.xml -swf-version 11 -swf bin/docs.swf
+haxe -lib hexunit  -lib hexdsl -lib hexcommand --macro "include('hex')" -D doc-gen --no-output -xml xml/php.xml -php bin
+haxe -lib hexunit  -lib hexdsl -lib hexcommand --macro "include('hex')" -D doc-gen --no-output -xml xml/neko.xml -neko bin/docs.n
+haxe -lib hexunit  -lib hexdsl -lib hexcommand --macro "include('hex')" -D doc-gen --no-output -xml xml/js.xml -js bin/docs.js
+haxe -lib hexunit  -lib hexdsl -lib hexcommand --macro "include('hex')" -D doc-gen --no-output -xml xml/flash.xml -swf-version 11 -swf bin/docs.swf
 
 haxelib install dox
 
